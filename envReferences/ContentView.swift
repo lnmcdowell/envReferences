bf818ace37655248aee302e4a6b9b294d4f90068
nathanielmcdowell <nathaniel29651@gmail.com>
2020-10-21 19:04:19 -0400

    Examples to move Environment Object between views.  Note that with sheets and drag/dismiss games, you do need the parent view of the sheet to pass in EnvironmentObject to it as well as it has a reference within itself. It seems you can skip view within the "inheritance" chain if it's not a sheet.  Even worked with sheets as long as you don't partially dismiss the sheet with a small drag and try to keep editing etc.  Neater code potential for not having to explicitly pass environment object to every intermediate view.

--- envReferences/ContentView.swift
+++ envReferences/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    //1
+    @EnvironmentObject var dsepCopy:testObject
+    @State private var isShown:Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //Text("Hello, world! - from \(myObj.name)")
+          //  .padding()
+        VStack{
+        statusView()
+        Button("Launch", action: {
+            print("works")
+            isShown = true
+        }).sheet(isPresented: $isShown, onDismiss: {print("dismissed")}, content: {
+            sheetView().environmentObject(dsepCopy) //2
+          
+        })
+        }
     }
 }
 
+/*
+ Note that 1 and 2 were needed in order to.... allow no crash if typing when closing or playing with partial drag of sheet
+ Without this, you get error message that
+ Thread 1: Fatal error: No ObservableObject of type testObject found.
+ A View.environmentObject(_:) for testObject may be missing
+ as an ancestor of this view.
+ Works fine if you dismiss it completely with drag or button, but crashes if you "Played" with dismiss drag,
+ but didn't complete and attempt to continue typing in the textfield
+ */
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(testObject())
+    }
+}
+
+struct sheetView:View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var localObj:testObject
+    //@ObservedObject var localObj:testObject
+    var body: some View {
+    Text("I'll have a burrito!")
+    //Toggle("Move the parent's Bool", isOn: <#T##Binding<Bool>#>)
+    TextField("my name is:", text: $localObj.name)
+    Button("Dismiss Me!!!",action: {
+        self.presentationMode.wrappedValue.dismiss()
+    })
+    }
+}
+
+struct statusView:View {
+    @EnvironmentObject var statObj:testObject
+    var body: some View{
+        Text("Hey yall from \(statObj.name)")
+            .padding()
+            .border(Color.red, width: 3)
     }
 }
---
+++ envReferences/testObject.swift
@@ -0,0 +1,14 @@
+//
+//  testObject.swift
+//  envReferences
+//
+//  Created by Nathaniel Mcdowell on 10/21/20.
+//
+
+import Foundation
+import SwiftUI
+
+class testObject:ObservableObject {
+    @Published var num:Int = 0
+    @Published var name:String = ""
+}
